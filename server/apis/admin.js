c1b56763658e026224ab7d0459b9f849023ad30a
Mark Kenneth Calendario <markcalendario@gmail.com>
2022-09-14 20:03:02 +0800

    create admin authentication

--- server/apis/admin.js
+++ server/apis/admin.js
@@ -2,25 +2,39 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const { jwtVerify } = require('../functions/jwt')
+
+async function verifyJWTToken(req, res, next) {
+    const isAuth = await jwtVerify(req.cookies.auth)
+    if (!isAuth) return res.send({ isAuth: false, message: "Auth expired." })
+    next()
+}
 
 router.post('/login', async (req, res) => {
-    await new Promise(resolve => {
-        bcrypt.compare(req.body.password, process.env.HASHED_PASSWORD, (error, same) => {
-            if (!same) {
-                return res.send({error: true, message: 'Incorrect Password.'});
-            }
 
+    // The Login API 
+    // It requires admin to input the unhashed bcrypt secret key to the input field
+    // Then the unhash will compare to the hashed password saved from .env
+    // If match, then sign a jwt then store to the client's pc
+    await new Promise(resolve => {
+        bcrypt.compare(req.body.password, process.env.LOGIN_SECRET_KEY, (error, same) => {
+
+            if (!same) return res.send({ error: true, message: 'Incorrect Password.' });
             resolve();
         });
     })
 
     let cookieValue = await new Promise(resolve => {
-        jwt.sign({ hashpass: process.env.HASHED_PASSWORD }, process.env.JWT_SECRET, { expiresIn: '1h' }, function(err, token) {
+        jwt.sign({ hashpass: process.env.LOGIN_SECRET_KEY }, process.env.JWT_SECRET, { expiresIn: '1h' }, function (err, token) {
             resolve(token);
         });
     })
 
-    return res.cookie('at', cookieValue, { httpOnly: true }).send({ error: false });
+    return res.cookie('auth', cookieValue, { httpOnly: true }).send({ error: false });
+})
+
+router.get('/auth', verifyJWTToken, async (req, res) => {
+    res.send({ isAuth: true, message: "Authenticated" })
 })
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const app = express();
+const cookies = require('cookie-parser')
+const bodyParser = require('body-parser')
 
 require('dotenv').config({ path: './.env.local' });
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+
+app.use(cookies());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const cors = require('cors');
 app.use(cors({ credentials: true, origin: process.env.CLIENT_URL }));
@@ -17,5 +21,7 @@ app.listen(process.env.PORT || 7000, async () => {
 // Routes
 const projects = require('./apis/projects');
 const admin = require('./apis/admin');
+const emailService = require('./apis/email-service');
 app.use('/projects', projects);
+app.use('/email-serve', emailService);
 app.use('/admin', admin);
